eead5b3c65174e4c5c91455e5b8c67e31c11208a
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-12-05 02:21:20 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -2,36 +2,36 @@ class Board
    attr_accessor :cells
 
    def initialize
-     self.cells = Array.new(9, " ")
+     @cells = Array.new(9, " ")
    end
 
    def reset!
-     self.cells = Array.new(9, " ")
+     @cells = Array.new(9, " ")
    end
 
    def display
-     puts " #{self.cells[0]} | #{self.cells[1]} | #{self.cells[2]} "
+     puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
      puts "-----------"
-     puts " #{self.cells[3]} | #{self.cells[4]} | #{self.cells[5]} "
+     puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
      puts "-----------"
-     puts " #{self.cells[6]} | #{self.cells[7]} | #{self.cells[8]} "
+     puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
    end
 
    def input_to_index(input)
      "#{input}".to_i - 1
    end
 
-   def position(input)
-     self.cells[input.to_i - 1]
+   def position(move)
+     @cells[move.to_i - 1]
    end
 
    def full?
-   self.cells.all?{|index| index == "X" || index == "O"}
+     @cells.all?{|index| index == "X" || index == "O"}
    end
 
    def turn_count
      counter = 0
-     self.cells.each do |index|
+     @cells.each do |index|
        if index == "X" || index == "O"
          counter += 1
        end
@@ -39,21 +39,21 @@ class Board
      counter
    end
 
-   def taken?(index)
-     !(self.cells[index.to_i - 1].nil? || self.cells[index.to_i - 1] == " ")
+   def taken?(move)
+     !(@cells[move.to_i - 1].nil? || @cells[move.to_i - 1] == " ")
    end
 
-   def valid_move?(index)
-     if taken?(index)
+   def valid_move?(move)
+     if taken?(move)
        puts "The position must be vacant, not currently taken by a player."
-     elsif !(index.to_i - 1).between?(0, 8)
+     elsif !(move.to_i - 1).between?(0, 8)
        puts "You must move to a position within the tic-tac-toe board."
      else false
        true
      end
    end
 
-   def update(index, player)
-     self.cells[index.to_i - 1] = player.token
+   def update(move, player)
+     @cells[move.to_i - 1] = player.token
    end
  end
